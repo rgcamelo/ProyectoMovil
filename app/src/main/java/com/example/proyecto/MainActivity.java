d2666cef72405ace9d3c2026bd8159f28a9ea194
rgcamelo <50896132+rgcamelo@users.noreply.github.com>
2020-05-04 13:07:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.proyecto.views.ContainerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent( this, ContainerActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto/views/ContainerActivity.java
@@ -0,0 +1,53 @@
+package com.example.proyecto.views;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.proyecto.R;
+import com.example.proyecto.views.fragments.HomeFragment;
+import com.example.proyecto.views.fragments.ProfileFragment;
+import com.example.proyecto.views.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+                showSelectedFragment(selectedFragment);
+                return true;
+            }
+        });
+    }
+
+    private void showSelectedFragment( Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_navigation,fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
+}
