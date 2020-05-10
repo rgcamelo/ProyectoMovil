93510ca436666d69c104901ff7df43fe041098f0
rgcamelo <50896132+rgcamelo@users.noreply.github.com>
2020-05-10 02:24:28 -0500

    CardViewActualizacion

--- app/src/main/java/com/example/proyecto/views/fragments/MapFragment.java
+++ app/src/main/java/com/example/proyecto/views/fragments/MapFragment.java
@@ -9,21 +9,50 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.proyecto.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
 
     public MapFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        getMapAsync(this);
+
+        return rootView;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public void onMapReady(GoogleMap map) {
+        // Posicionar el mapa en una localización y con un nivel de zoom
+        LatLng latLng = new LatLng( 9.333342, -73.500009);
+        // Un zoom mayor que 13 hace que el emulador falle, pero un valor deseado para
+        // callejero es 17 aprox.
+        float zoom = 13;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,8));
+
+        // Colocar un marcador en la misma posición
+        map.addMarker(new MarkerOptions().position(latLng));
+        // Más opciones para el marcador en:
+        // https://developers.google.com/maps/documentation/android/marker
+        // http://www.hermosaprogramacion.com/2016/05/google-maps-android-api-v2/
+        //https://javiergarciaescobedo.es/desarrollo-android/82-interfaz-de-usuario/433-mapa-de-google-como-fragment-en-aplicacion-android
+
+        // Otras configuraciones pueden realizarse a través de UiSettings
+        // UiSettings settings = getMap().getUiSettings();
     }
+
+
 }
